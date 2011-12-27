1b514b2293c336c7260a11818e19888b6da6fe6e
Larry Kang <larry@cotweet.com>
2011-12-27 00:13:11 -0800

    port 80 for no.de

--- server.js
+++ server.js
@@ -100,8 +100,8 @@ app.get('/', function(req, res) {
 })
 
 
-port = process.env.PORT || 3000 // for heroku
-// port = 80 // for no.de
+// port = process.env.PORT || 3000 // for heroku
+port = 80 // for no.de
 app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
